33a9561e44a052154a40f9604e69a7503d14d8cc
Eric Johnson <eric.johnson@pumascan.com>
2018-10-05 13:11:25 -0500

    Minor launch script update.

--- build/launch.sh
+++ build/launch.sh
@@ -2,9 +2,9 @@
 
 #Spin up temp mysql container
 docker pull mysql:latest
-docker run --name creditunion-db-acceptance -e MYSQL_ROOT_PASSWORD='dev$3c0psForTheWin' -e MYSQL_DATABASE='CreditUnion' -e MYSQL_USER='credituser' -e MYSQL_PASSWORD='dev$3c0psForTheWin' -d mysql:latest
+docker run --name creditunion-db-$1 -e MYSQL_ROOT_PASSWORD='dev$3c0psForTheWin' -e MYSQL_DATABASE='CreditUnion' -e MYSQL_USER='credituser' -e MYSQL_PASSWORD='dev$3c0psForTheWin' -d mysql:latest
 
-CONTAINER_ID=$(docker ps | grep "creditunion-db-acceptance" | awk '{print $1}')
+CONTAINER_ID=$(docker ps | grep "creditunion-db-$1" | awk '{print $1}')
 IP_ADDRESS=$(docker inspect $CONTAINER_ID | jq -r '.[].NetworkSettings.Networks[].IPAddress')
 
 # UPDATE APPSETTINGS JSON WITH IP ADDRESS TO CONNECT TO MYSQL
@@ -12,7 +12,7 @@ cd src/app/
 sed -i -e "s#Server=localhost;#Server=$IP_ADDRESS;#g" api/appsettings.json
 
 # Build the testing container
-docker build --tag $1:$2 .
+docker build --tag $2:$3 .
 
 # Run web api container
-docker run --name creditunion-api-acceptance -p 44300:44300 --link creditunion-db-acceptance -d $1:$2
+docker run --name creditunion-api-$1 -p 44300:44300 --link creditunion-db-$1 -d $2:$3
